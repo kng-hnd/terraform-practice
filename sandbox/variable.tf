aeb8c635298d15fb20019b719abd2e984c6ae5f6
kng-hnd <76693564+kng-hnd@users.noreply.github.com>
2021-01-01 00:02:06 +0900

    [add] creating network resources

---
+++ sandbox/variable.tf
@@ -0,0 +1,14 @@
+locals {
+  project_name = "example"
+
+  vpc_cidr = {
+    stg = "10.0.0.0/16"
+    prd = "10.1.0.0/16"
+  }
+
+ subnet_numbers = {
+    "ap-northeast-1a" = 0
+    "ap-northeast-1c" = 1
+    "ap-northeast-1d" = 2
+  }
+}
